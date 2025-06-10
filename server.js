e1244428692e8bc3e9654709da6d79472c8dde63
cabbaj <ppvadon@gmail.com>
2025-06-10 13:38:37 +0700

    seperate files

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
-
 const port = 3000;
 
 app.use(express.json());
-
-app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("hee");
